1b6e9ff76b79391a641dfb5a3c434a2963ba1eac
abdulloooh <abdullahakinwumi@gmail.com>
2020-08-05 01:52:06 +0100

    implemented fetching directories, next is making it recursive for dynamism

---
+++ fetch.php
@@ -0,0 +1,25 @@
+<?php
+$topDir = './modules';
+$classTree = array_flip( array_values( array_diff( scandir( $topDir ), array( '.', '..' ) ) ) );
+
+foreach ( $classTree as $key => $value ) {
+    $lowDir = "$topDir/{$key}";
+    $under = array_values( array_diff( scandir( $lowDir ), array( '.', '..' ) ) );
+    if ( is_dir( "{$lowDir}/{$under[0]}" ) === false ) {
+        $classTree[$key] = $under;
+    } else {
+        $list = array_flip( $under );
+        $classTree[$key] = $list;
+        foreach ( $classTree[$key] as $k => $value ) {
+            $lowerDir = "$lowDir/{$k}";
+            $underUnder = array_values( array_diff( scandir( $lowerDir ), array( '.', '..' ) ) );
+            if ( is_dir( $underUnder[0] ) === false ) {
+                $classTree[$key][$k] = $underUnder;
+            } else {
+
+            }
+        }
+    }
+}
+
+die( var_dump( $classTree ) );
